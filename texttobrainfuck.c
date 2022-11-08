b092ab3e32bc6959a01b289cf0e28487125a2427
dblourens <113265830+dblourens@users.noreply.github.com>
2022-11-08 18:18:32 +0100

    Add files via upload

---
+++ texttobrainfuck.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+    char input;
+    int intInput, intInput2, temp1 = 0, shortened;
+    while(scanf("%c", &input) != EOF) {
+        intInput = (int)input;
+        intInput2 = intInput;
+        if (intInput < 10) {
+            printf("Invalid Input\n");
+            break;
+        }
+        temp1 = intInput/10;
+        if (temp1 > 0) {
+            printf(">");
+        }
+        for (int i = 0; temp1 > i; i++) {
+            printf("+");
+            shortened = 1;
+        }
+        if (shortened) {
+            printf("[");
+            printf("<");
+        }
+        if (shortened) {
+            for (int i = 0; 10 > i; i++) {
+                printf("+");
+            }
+        }
+        if (shortened) {
+            printf(">");
+            printf("-");
+            printf("]");
+            printf("<");
+            intInput = intInput - temp1*10;
+        }
+        for (int i = 0; intInput > i; i++) {
+            printf("+");
+        }
+        printf(".");
+        printf(">");
+        if (intInput2==10) {
+            printf("\n");
+        }
+    }
+    return 0;
+}
